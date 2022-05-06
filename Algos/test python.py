40c4522403dcb0a0c474a1942b720e2416b32336
Paulo <105054182+PauloMoq@users.noreply.github.com>
2022-05-06 15:06:49 +0200

    Delete Algos directory

--- Algos/test python.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__" :
-    tableau = []
-    for i in range(10) :
-        dico = []
-        for j in range(10) :
-            dico.append({'contenu' : 'Eau', 'numero' : 0, 'etat' : 'Neuf'})
-        tableau.append(dico)
-
-    #Dans chaque case du tableau, on met le dico ci-dessus
